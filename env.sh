0e7f45627f22f7c82526e68c5baa669ae7c3f8e9
mprokunin <56938241+mprokunin@users.noreply.github.com>
2026-06-26 15:21:06 +0500

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,10 @@
+# Python2
+python2 -m pip install --user virtualenv
+virtualenv -p /usr/bin/python2 my_py2_env
+
+source my_py2_env/bin/activate
+
+# Python3
+python3 -m venv my_py3_env
+
+source my_py3_env/bin/activate
